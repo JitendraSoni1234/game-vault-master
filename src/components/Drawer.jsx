ddd8a5a608fd63c670782363cb3bb21d7c9a6764
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2023-05-02 15:23:15 +0530

    code fix

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -14,14 +14,20 @@ function Drawer({ title = 'demo', open = false, setOpen, children }) {
   }, [drawerRef, setOpen]);
 
   return (
-    <div ref={drawerRef} className={`fixed right-0 bg-white h-full top-0 transition duration-75 ease-in z-50 w-[25vw] shadow-[-10px_0_10px_rgba(0,0,0,0.1)] ${!open ? 'translate-x-[100%]' : ''}`}>
-      <div className='dark:bg-gray-800 h-[80px] text-gray-600 flex place-items-center justify-between dark:text-white text-3xl px-10 shadow-[0_10px_10px_rgba(0,0,0,0.1)]'>
-        <span>{title}</span>
-        <button className='focus:outline-none' onClick={() => setOpen(i => !i)}>
-          X
-        </button>
-      </div>
-      <div className=''>{children}</div>
+    <div
+      ref={drawerRef}
+      className={`fixed right-0 bg-white h-full top-0 transition duration-75 ease-in z-50 w-[25vw] shadow-[-10px_0_10px_rgba(0,0,0,0.1)] ${!open ? 'translate-x-[100%]' : ''}`}>
+      {open && (
+        <>
+          <div className='dark:bg-gray-800 h-[80px] text-gray-600 flex place-items-center justify-between dark:text-white text-3xl px-10 shadow-[0_10px_10px_rgba(0,0,0,0.1)]'>
+            <span>{title}</span>
+            <button className='focus:outline-none' onClick={() => setOpen(i => !i)}>
+              X
+            </button>
+          </div>
+          <div className=''>{children}</div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,5 +1,13 @@
+import { useSearchParams } from 'react-router-dom';
+
 function FilterComponent() {
-  return <div className="p-5 text-center text-xl">filter feature comming soon...</div>;
+  const [params, setParams] = useSearchParams();
+
+  return (
+    <div className='p-5 text-center text-xl' onClick={() => setParams({ plate: 'pc' })}>
+      {params.get('plate')} filter feature coming soon...
+    </div>
+  );
 }
 
 export default FilterComponent;
